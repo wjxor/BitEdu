f25672603ed63bba6957b211bfe1eb22fd28d615
wjxor <shko940575@gmail.com>
2021-09-02 11:16:47 +0900

    tri4 수정

--- src/Quiz/Quiz_09_01.java
+++ src/Quiz/Quiz_09_01.java
@@ -147,11 +147,12 @@ public class Quiz_09_01 {
 	*/
 	private static void tri4() {
 		for (int i = 1; i <= 4; i++) {
-			for (int j = 1; j <= 5; j++) {
-				if (i < j) {
-					System.out.print("*");
-				} else
-					System.out.print(" ");
+			for (int j = 1; j < i; j++) {
+				System.out.print(" ");
+			}
+
+			for (int k = 4; k >= i; k--) {
+				System.out.print("*");
 			}
 
 			System.out.println();
