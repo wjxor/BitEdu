2d80f35693aafe66270bd635c006b4f27700c23a
wjxor <shko940575@gmail.com>
2021-09-07 17:47:32 +0900

    오후 + 자습

--- src/ex09_07/sec12.java
+++ src/ex09_07/sec12.java
@@ -3,8 +3,40 @@ package ex09_07;
 public class sec12 {
 
 	public static void main(String[] args) {
-		System.out.println(args[]);
+		Week today = Week.FRIDAY;
+		System.out.println(today);
 
+		// name() 메서드
+		String name = today.name();
+		System.out.println(name);
+
+		int ordinal = today.ordinal();
+		System.out.println(ordinal);
+
+		// compareTo() 메소드
+		Week day1 = Week.MONDAY;
+		Week day2 = Week.WEDNESDAY;
+		int result1 = day1.compareTo(day2);
+		int result2 = day2.compareTo(day1);
+		System.out.println(result1);
+		System.out.println(result2);
+
+		Week weekDay = Week.valueOf("SATURDAY");
+		if (weekDay == Week.SATURDAY || weekDay == Week.SUNDAY) {
+			System.out.println("주말 이군요");
+		} else {
+			System.out.println("평일 이군요");
+		}
+
+		// values() 메서드
+		Week[] days = Week.values();
+		for (Week day : days) {
+			System.out.println(day);
+		}
 	}
 
+}
+
+enum Week {
+	MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
 }
