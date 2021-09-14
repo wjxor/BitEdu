71b426e7889c41241be9bb0a18dc4c300029f3bd
wjxor <shko940575@gmail.com>
2021-09-14 17:47:21 +0900

    9/14

---
+++ src/ex09_14/Sec16.java
@@ -0,0 +1,5 @@
+package ex09_14;
+
+public class Sec16 {
+
+}
---
+++ src/ex09_14/Summary09_14.java
@@ -0,0 +1,10 @@
+package ex09_14;
+
+public class Summary09_14 {
+	// 부모에서 private 자식이 사용 X
+	// super -> 부모의 객체 접근
+	// -> super.print() - 메서드 super() - 생성자
+	// 부모의 메서드를 재정의 -> 오버라이딩
+	// @Override -> 오버라이딩이 잘되었는지 컴파일러가 확인
+
+}
