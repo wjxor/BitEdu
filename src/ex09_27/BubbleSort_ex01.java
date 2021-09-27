85fab84a182b8a809b4d15d391f9a49204ef23d1
wjxor <shko940575@gmail.com>
2021-09-27 12:54:02 +0900

    오전

---
+++ src/ex09_27/BubbleSort_ex01.java
@@ -0,0 +1,29 @@
+package ex09_27;
+
+import java.util.Arrays;
+
+public class BubbleSort_ex01 {
+	public static void main(String[] args) {
+		// 버블정렬
+		// -> 맨 마지막부터 고정(최소값, 최대값)
+		// -> 두자리씩 비교
+		// -> n-1번 반복
+		int[] arr = { 8, 2, 14, 7, 5 };
+		BubbleSort(arr);
+	}
+
+	private static void BubbleSort(int[] arr) {
+		int temp = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 1; j < arr.length - i; j++) {
+				if (arr[j - 1] > arr[j]) {
+					temp = arr[j - 1];
+					arr[j - 1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+
+		System.out.println(Arrays.toString(arr));
+	}
+}
